14cc0d90aac4a877aa058b8e752559b826671aee
Elvis Stiglic <41340032+NukeTurtle@users.noreply.github.com>
2021-05-17 11:19:08 +0100

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,8 @@
+function bubble(){
+    let bubble = document.querySelector(".icons");
+    if (bubble.style.display === "none") {
+        bubble.style.display = "block";
+    } else {
+    bubble.style.display = "none";
+    }
+}
